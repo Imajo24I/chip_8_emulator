c5160762b0362421645dd98d44017b98426dea0f
Martin <martin.j.schilling@gmx.de>
2024-11-06 19:13:24 +0100

    add more chip 8 components

--- src/emulator/emulator.rs
+++ src/emulator/emulator.rs
@@ -1,13 +1,44 @@
+use std::error::Error;
 use std::path::PathBuf;
 
 pub struct Chip8Emulator {
-    pub display: [[bool; 64]; 32]
+    pub display: [[bool; 64]; 32],
+
+    // Program Counter
+    // Used to store location of the next instruction
+    pc: usize,
+
+    // Index Register
+    // Used to point at locations in memory
+    i_register: u16,
+
+    // Stack
+    // Used to call and return from subroutines (functions)
+    stack: Vec<u16>,
+
+    // General-purpose variable registers
+    v_registers: [u8; 16],
+
+    // Delay Timer
+    // Decrements 60 times per second
+    delay_timer: u8,
+
+    // Sound Timer
+    // Functions like the Delay Timer, however also gives of a beep sounds when not 0
+    sound_timer: u8,
 }
 
 impl Chip8Emulator {
     pub fn new(filepath: PathBuf) -> Self {
         Self {
-            display: [[false; 64]; 32]
+            display: [[false; 64]; 32],
+            // Start at 0x200, since 0x000 - 0x1FF are reserved for interpreter
+            pc: 0x200,
+            i_register: 0,
+            stack: Vec::new(),
+            v_registers: [0; 16],
+            delay_timer: 0,
+            sound_timer: 0,
         }
     }
 }
