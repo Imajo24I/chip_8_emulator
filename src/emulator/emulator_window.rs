3082999519036210e97c4c1214a9054895ecb493
Martin <martin.j.schilling@gmx.de>
2024-11-14 20:37:04 +0100

    make emulator display dynamically fit to screen

--- src/emulator/emulator_window.rs
+++ src/emulator/emulator_window.rs
@@ -5,8 +5,6 @@ use eframe::egui::{Pos2, Ui};
 use crate::errors::error::Error;
 use std::path::Path;
 
-const PIXEL_WIDTH: f32 = 20f32;
-
 pub struct EmulatorWindow {
     emulator: Emulator,
 }
@@ -19,14 +17,18 @@ impl EmulatorWindow {
     }
 
     pub fn update(&mut self, ui: &mut Ui) -> Option<Event> {
+        let window_size = ui.ctx().input(|i| i.viewport().inner_rect.unwrap().size());
+        let pixel_width = window_size.x / 64f32;
+        let pixel_height = window_size.y / 32f32;
+
         // Draw Display
         for (row_index, row) in self.emulator.display.iter().enumerate() {
             for (pixel_index, pixel) in row.iter().enumerate() {
-                let pixel_pos = Pos2::new(pixel_index as f32 * PIXEL_WIDTH, row_index as f32 * PIXEL_WIDTH);
+                let pixel_pos = Pos2::new(pixel_index as f32 * pixel_width, row_index as f32 * pixel_height);
 
                 let rect = egui::Rect::from_two_pos(
                     pixel_pos,
-                    pixel_pos + egui::vec2(PIXEL_WIDTH, PIXEL_WIDTH),
+                    pixel_pos + egui::vec2(pixel_width + 1f32, pixel_height + 1f32),
                 );
 
                 let rect_color = if *pixel {
