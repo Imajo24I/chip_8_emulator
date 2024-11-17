5786ad5fd3cd13f7df4f26060c7779b56c698b4d
Martin <martin.j.schilling@gmx.de>
2024-11-17 19:48:19 +0100

    small cleanup

--- src/emulator/instructions.rs
+++ src/emulator/instructions.rs
@@ -67,7 +67,6 @@ pub fn execute_instruction(emulator: &mut Emulator, opcode: u16) -> Option<Event
 fn x_dxyn(emulator: &mut Emulator, opcode: u16) -> Option<Event> {
     // DXYN - Draw sprite at coordinate VX, VY with N bytes of sprite data
 
-    // Get X and Y coordinates, which wrap around screen
     let vx = ((opcode & 0x0F00) >> 8) as usize;
     let vy = ((opcode & 0x00F0) >> 4) as usize;
 
@@ -79,8 +78,8 @@ fn x_dxyn(emulator: &mut Emulator, opcode: u16) -> Option<Event> {
     let x = (emulator.v_registers[vx] & 63) as usize;
     let y = (emulator.v_registers[vy] & 31) as usize;
 
-    // Set VF to 0
     emulator.v_registers[0xF] = 0;
+
 
     let n = (opcode & 0x000F) as usize;
 
