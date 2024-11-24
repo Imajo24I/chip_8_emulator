5c96ebe69bcf796ebda69e1aff6bad6084dc7a29
Martin <martin.j.schilling@gmx.de>
2024-11-24 18:51:21 +0100

    fix DXYN - incorrectly flipped the pixels

--- src/emulator/instructions/x_dxyn.rs
+++ src/emulator/instructions/x_dxyn.rs
@@ -5,42 +5,48 @@ use crate::events::Event;
 pub fn x_dxyn(emulator: &mut Emulator, opcode: u16) -> Result<(), Event> {
     // DXYN - Draw sprite at coordinate VX, VY with N bytes of sprite data
 
-    let vx = ((opcode & 0x0F00) >> 8) as usize;
-    let vy = ((opcode & 0x00F0) >> 4) as usize;
-
-    for v in [vx, vy].iter() {
-        validate_v_reg_index(*v, opcode, emulator)?;
-    }
-
-    let x = (emulator.v_registers[vx] & 63) as usize;
-    let y = (emulator.v_registers[vy] & 31) as usize;
+    let (x, y, height) = get_x_y_height(emulator, opcode)?;
 
     emulator.v_registers[0xF] = 0;
 
-
-    let n = (opcode & 0x000F) as usize;
-
-    for row in 0..n {
+    for row in 0..height {
         let sprite_data = emulator.memory[emulator.i_register + row];
-        let mut bit_x = x;
-        for bit in (0..8).rev() {
-            let current_bit = (sprite_data >> bit) & 1;
-            emulator.display[y + row][bit_x] = current_bit == 1;
 
-            if current_bit == 1 {
-                emulator.v_registers[0xF] = 1;
+        for bit in 0..8 {
+            if (sprite_data & (0x80 >> bit)) != 0 {
+                let x_coord = x + bit;
+                let y_coord = y + row;
+
+                if x_coord <= 64 || y_coord <= 32 {
+                    flip_pixel(x_coord, y_coord, emulator);
+                }
             }
-
-            bit_x += 1;
-            if bit_x == 64 {
-                break;
-            }
-        }
-
-        if y + row == 32 {
-            break;
         }
     }
 
     Ok(())
+}
+
+fn flip_pixel(x_coord: usize, y_coord: usize, emulator: &mut Emulator) {
+    if emulator.display[y_coord][x_coord] {
+        emulator.v_registers[0xF] = 1;
+        emulator.display[y_coord][x_coord] = false;
+    } else {
+        emulator.display[y_coord][x_coord] = true;
+    }
+}
+
+fn get_x_y_height(emulator: &mut Emulator, opcode: u16) -> Result<(usize, usize, usize), Event> {
+    let vx = ((opcode & 0x0F00) >> 8) as usize;
+    validate_v_reg_index(vx, opcode, emulator)?;
+
+    let vy = ((opcode & 0x00F0) >> 4) as usize;
+    validate_v_reg_index(vy, opcode, emulator)?;
+
+    let x = (emulator.v_registers[vx] & 63) as usize;
+    let y = (emulator.v_registers[vy] & 31) as usize;
+
+    let height = (opcode & 0x000F) as usize;
+
+    Ok((x, y, height))
 }
