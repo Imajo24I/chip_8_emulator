5a9a69d3aa6742d55ed48287599366bc885afd9e
Martin <martin.j.schilling@gmx.de>
2024-11-30 13:33:30 +0100

    fix crash when window is minimized

--- src/chip_8/emulator_window.rs
+++ src/chip_8/emulator_window.rs
@@ -35,29 +35,33 @@ impl EmulatorWindow {
         }
 
         // Draw Display
-        let window_size = ui.ctx().input(|i| i.viewport().inner_rect.unwrap().size());
-        let pixel_width = window_size.x / 64f32;
-        let pixel_height = window_size.y / 32f32;
+        let inner_rect = ui.ctx().input(|i| i.viewport().inner_rect);
 
-        for (row_index, row) in self.emulator.display.iter().enumerate() {
-            for (pixel_index, pixel) in row.iter().enumerate() {
-                let pixel_pos = Pos2::new(
-                    pixel_index as f32 * pixel_width,
-                    row_index as f32 * pixel_height,
-                );
+        if let Some(inner_rect) = inner_rect {
+            let window_size = inner_rect.size();
+            let pixel_width = window_size.x / 64f32;
+            let pixel_height = window_size.y / 32f32;
 
-                let rect = egui::Rect::from_two_pos(
-                    pixel_pos,
-                    pixel_pos + egui::vec2(pixel_width + 1f32, pixel_height + 1f32),
-                );
+            for (row_index, row) in self.emulator.display.iter().enumerate() {
+                for (pixel_index, pixel) in row.iter().enumerate() {
+                    let pixel_pos = Pos2::new(
+                        pixel_index as f32 * pixel_width,
+                        row_index as f32 * pixel_height,
+                    );
 
-                let rect_color = if *pixel {
-                    egui::Color32::WHITE
-                } else {
-                    egui::Color32::BLACK
-                };
+                    let rect = egui::Rect::from_two_pos(
+                        pixel_pos,
+                        pixel_pos + egui::vec2(pixel_width + 1f32, pixel_height + 1f32),
+                    );
 
-                ui.painter().rect_filled(rect, 0f32, rect_color);
+                    let rect_color = if *pixel {
+                        egui::Color32::WHITE
+                    } else {
+                        egui::Color32::BLACK
+                    };
+
+                    ui.painter().rect_filled(rect, 0f32, rect_color);
+                }
             }
         }
 
