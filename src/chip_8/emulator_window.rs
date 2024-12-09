66241f9a823e403deae7a730e5688198f649fc02
Martin <martin.j.schilling@gmx.de>
2024-12-09 13:03:41 +0100

    refactor

--- src/chip_8/emulator_window.rs
+++ src/chip_8/emulator_window.rs
@@ -1,11 +1,11 @@
-use crate::chip_8::emulator::Emulator;
 use crate::chip_8::config::Config;
+use crate::chip_8::emulator::Emulator;
 use crate::events::Event;
+use anyhow::Error;
 use eframe::egui;
-use eframe::egui::{Pos2, Ui};
+use eframe::egui::{Pos2, Rect, Ui, Vec2};
 use std::path::Path;
 use std::time::{Duration, Instant};
-use anyhow::Error;
 
 pub struct EmulatorWindow {
     emulator: Emulator,
@@ -39,40 +39,38 @@ impl EmulatorWindow {
             }
         }
 
-        // Draw Display
         let inner_rect = ui.ctx().input(|i| i.viewport().inner_rect);
-
         if let Some(inner_rect) = inner_rect {
-            let window_size = inner_rect.size();
-            let pixel_width = window_size.x / 64f32;
-            let pixel_height = window_size.y / 32f32;
-
-            for (row_index, row) in self.emulator.display.iter().enumerate() {
-                let pixel_y = row_index as f32 * pixel_height;
-
-                for (pixel_index, pixel) in row.iter().enumerate() {
-                    let pixel_pos = Pos2::new(
-                        pixel_index as f32 * pixel_width,
-                        pixel_y,
-                    );
-
-                    let rect = egui::Rect::from_two_pos(
-                        pixel_pos,
-                        pixel_pos + egui::vec2(pixel_width + 1f32, pixel_height + 1f32),
-                    );
-
-                    let rect_color = if *pixel {
-                        egui::Color32::WHITE
-                    } else {
-                        egui::Color32::BLACK
-                    };
-
-                    ui.painter().rect_filled(rect, 0f32, rect_color);
-                }
-            }
+            self.draw_display(inner_rect.size(), ui);
         }
 
         None
+    }
+
+    fn draw_display(&mut self, window_size: Vec2, ui: &mut Ui) {
+        let pixel_width = window_size.x / 64f32;
+        let pixel_height = window_size.y / 32f32;
+
+        for (row_index, row) in self.emulator.display.iter().enumerate() {
+            let pixel_y = row_index as f32 * pixel_height;
+
+            for (pixel_index, pixel) in row.iter().enumerate() {
+                let pixel_pos = Pos2::new(pixel_index as f32 * pixel_width, pixel_y);
+
+                let rect = egui::Rect::from_two_pos(
+                    pixel_pos,
+                    pixel_pos + egui::vec2(pixel_width + 1f32, pixel_height + 1f32),
+                );
+
+                let rect_color = if *pixel {
+                    egui::Color32::WHITE
+                } else {
+                    egui::Color32::BLACK
+                };
+
+                ui.painter().rect_filled(rect, 0f32, rect_color);
+            }
+        }
     }
 
     fn wait_for_next_frame(&mut self) {
